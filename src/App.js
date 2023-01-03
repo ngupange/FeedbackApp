fbdbe5657cee3e13649f6815e69c1cc2415270bf
bienvenu <gcab022120@coderacademy.edu.au>
2023-01-04 07:55:13 +1000

    CSS and Header component starting point

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+function App() {
+    // const title = 'Blog Post'
+    // const body = 'This is my blog post'
+
+
+    return (
+        <>
+            <Header text="Hallo World !" />
+            <div className="container">
+                <h1>My App</h1>
+            </div>
+        </>
+    )
+}
+export default App 
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+// ReactDOM.render(<h1>My App</h1>, document.getElementById('root'))
