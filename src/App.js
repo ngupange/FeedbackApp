6fb7776829e755605f8cd00dbc7e249f4a3baa46
bienvenu <gcab022120@coderacademy.edu.au>
2023-01-04 12:06:55 +1000

    Managing Global State

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
+import { useState } from "react"
 import Header from "./components/Header"
-import FeedbackItem from "./components/FeedbackItem"
+import FeedbackData from "./data/FeedbackData"
+import FeedbackList from "./components/FeedbackList"
 function App() {
-    // const title = 'Blog Post'
-    // const body = 'This is my blog post'
-
-
+    const [feedback, setFeedback] = useState(FeedbackData)
     return (
         <>
             <Header />
             <div className="container">
-                <FeedbackItem />
+                <FeedbackList feedback={feedback} />
             </div>
         </>
     )
--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-import React from 'react'
-
-function FeedbackItem() {
-    const [rating, setRating] = useState(7)
-    const [text, setText] = useState('This is an example of a feedback item !!')
+function FeedbackItem({ item }) {
     return (
         <div className='card'>
-            <div className="num-display">{rating}</div>
-            <div className="tetxt-display">{text}</div>
+            <div className="num-display">{item.rating}</div>
+            <div className="tetxt-display">{item.text}</div>
         </div>
     )
 }
