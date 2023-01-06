5d84f3b985b228dfed37fefb0cae3b16a73cc356
bienvenu <gcab022120@coderacademy.edu.au>
2023-01-06 22:32:09 +1000

    Real-Time Validation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/feedbackStats"
+import FeedbackForm from "./components/FeedbackForm"
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
     const deleteFeedback = (id) => {
@@ -14,6 +15,7 @@ function App() {
         <>
             <Header />
             <div className="container">
+                <FeedbackForm />
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
             </div>
