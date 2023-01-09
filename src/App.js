14099ee4d0c87ecd5d30f459706d0920502713af
bienvenu <gcab022120@coderacademy.edu.au>
2023-01-09 12:12:28 +1000

    Routes and Links"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/feedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
 
@@ -19,14 +22,24 @@ function App() {
         }
     }
     return (
-        <>
+        <Router>
             <Header />
             <div className="container">
-                <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                <Routes>
+                    <Route exact path='/' element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback} />
+                            <FeedbackStats feedback={feedback} />
+                            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                        </>
+                    }>
+
+                    </Route>
+                    <Route path='/about' element={<AboutPage />} />
+                </Routes>
+                <AboutIconLink />
             </div>
-        </>
+        </Router>
     )
 }
 export default App 
