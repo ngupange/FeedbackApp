8cbff4f95bd2b362ef08f2db301003ac04958960
bienvenu <gcab022120@coderacademy.edu.au>
2023-01-10 12:51:51 +1000

    Context

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import FeedbackStats from "./components/feedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
 import AboutIconLink from './components/AboutIconLink'
 import AboutPage from './pages/AboutPage'
+import { FeedbackProvider } from './context/FeedbackContext'
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
 
@@ -22,24 +23,27 @@ function App() {
         }
     }
     return (
-        <Router>
-            <Header />
-            <div className="container">
-                <Routes>
-                    <Route exact path='/' element={
-                        <>
-                            <FeedbackForm handleAdd={addFeedback} />
-                            <FeedbackStats feedback={feedback} />
-                            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-                        </>
-                    }>
+        <FeedbackProvider>
+            <Router>
+                <Header />
+                <div className="container">
+                    <Routes>
+                        <Route exact path='/' element={
+                            <>
+                                <FeedbackForm handleAdd={addFeedback} />
+                                <FeedbackStats />
+                                <FeedbackList handleDelete={deleteFeedback} />
+                            </>
+                        }>
 
-                    </Route>
-                    <Route path='/about' element={<AboutPage />} />
-                </Routes>
-                <AboutIconLink />
-            </div>
-        </Router>
+                        </Route>
+                        <Route path='/about' element={<AboutPage />} />
+                    </Routes>
+                    <AboutIconLink />
+                </div>
+            </Router>
+        </FeedbackProvider>
     )
 }
-export default App 
+export default App
+
