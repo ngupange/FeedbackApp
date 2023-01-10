620523971a3789548d8f5ff21fb88d51edbe941e
bienvenu <gcab022120@coderacademy.edu.au>
2023-01-10 15:52:44 +1000

    Update feedback item

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,16 +1,24 @@
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import RatingSelect from './RatingSelect'
 import Card from './shared/Card'
 import Button from './shared/Button'
 import FeedbackContext from '../context/FeedbackContext'
-
 function FeedbackForm() {
-    const { addFeedback } = useContext(FeedbackContext)
     const [text, setText] = useState('')
     const [rating, setRating] = useState(10)
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [message, setMessage] = useState('')
 
+    const { addFeedback, feedbackEdit, updateFeedback } =
+        useContext(FeedbackContext)
+
+    useEffect(() => {
+        if (feedbackEdit.edit === true) {
+            setBtnDisabled(false)
+            setText(feedbackEdit.item.text)
+            setRating(feedbackEdit.item.rating)
+        }
+    }, [feedbackEdit])
     const handleTextChange = (e) => {
         if (text === '') {
             setBtnDisabled(true)
@@ -24,7 +32,6 @@ function FeedbackForm() {
         }
         setText(e.target.value)
     }
-
     const handleSubmit = (e) => {
         e.preventDefault()
         if (text.trim().length > 10) {
@@ -33,13 +40,15 @@ function FeedbackForm() {
                 rating,
             }
 
-            addFeedback(newFeedback)
+            if (feedbackEdit.edit === true) {
+                updateFeedback(feedbackEdit.item.id, newFeedback)
+            } else {
+                addFeedback(newFeedback)
+            }
 
             setText('')
         }
     }
-
-
     return (
         <Card>
             <form onSubmit={handleSubmit}>
--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import { useState, useContext, useEffect } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
 function RatingSelect({ select }) {
     const [selected, setSelected] = useState(10)
+
+    const { feedbackEdit } = useContext(FeedbackContext)
+
+    useEffect(() => {
+        setSelected(feedbackEdit.item.rating)
+    }, [feedbackEdit])
 
     const handleChange = (e) => {
         setSelected(+e.currentTarget.value)
         select(+e.currentTarget.value)
     }
-
     return (
         <ul className='rating'>
             <li>
@@ -123,5 +129,4 @@ function RatingSelect({ select }) {
         </ul>
     )
 }
-
 export default RatingSelect
